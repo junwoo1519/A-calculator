1a9e80f84e0b8e5f55000ab433f01c368f494663
JUNU <junwoo101797@gmail.com>
2023-10-18 13:37:37 +0900

    번호 클릭시 스크린에 나오도록 수정

--- src/script.js
+++ src/script.js
@@ -1,2 +1,48 @@
-var calculatorScreen = document.querySelector(".calculator__header");
-calculatorScreen.textContent = "123456";
+var calculatorScreen = document.querySelector(".calculator__screen");
+var padButton = document.querySelectorAll(".calculator__table tbody td button");
+var result = 0;
+padButton.forEach(function (item, i) {
+    item.addEventListener("click", function (e) {
+        if (item.id === 'Num0') {
+            result = 0;
+            calculatorScreen.textContent = String(result);
+            // calculatorScreen.textContent = result.toString();
+        }
+        else if (item.id === 'Num1') {
+            result = 1;
+            calculatorScreen.textContent = String(result);
+        }
+        else if (item.id === 'Num2') {
+            result = 2;
+            calculatorScreen.textContent = String(result);
+        }
+        else if (item.id === 'Num3') {
+            result = 3;
+            calculatorScreen.textContent = String(result);
+        }
+        else if (item.id === 'Num4') {
+            result = 4;
+            calculatorScreen.textContent = String(result);
+        }
+        else if (item.id === 'Num5') {
+            result = 5;
+            calculatorScreen.textContent = String(result);
+        }
+        else if (item.id === 'Num6') {
+            result = 6;
+            calculatorScreen.textContent = String(result);
+        }
+        else if (item.id === 'Num7') {
+            result = 7;
+            calculatorScreen.textContent = String(result);
+        }
+        else if (item.id === 'Num8') {
+            result = 8;
+            calculatorScreen.textContent = String(result);
+        }
+        else if (item.id === 'Num9') {
+            result = 9;
+            calculatorScreen.textContent = String(result);
+        }
+    });
+});
