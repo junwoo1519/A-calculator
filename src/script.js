3e86225d6393b7f0d80c6dd0b1e3ad87a293f19c
JUNU <junwoo101797@gmail.com>
2023-10-20 16:57:16 +0900

    더하기 기능 작업중

--- src/script.js
+++ src/script.js
@@ -1,9 +1,39 @@
+// 스크린 영역
 var calculatorScreen = document.querySelector(".calculator__screen");
+var calculatorSubScreen = document.querySelector(".calculator__sub-screen");
+// 스크린 영역 안 버튼
 var padButton = document.querySelectorAll(".calculator__table tbody td button");
-var result = 0;
+// 총합
+var total = document.querySelector("#total");
+var allClearButton = document.querySelector("#AllClear");
+var A = 0;
+var B = 0;
+// 초기화
+var result = '';
+// 버튼 반복 돌리면서 클리이벤트 걸어주기
 padButton.forEach(function (item, i) {
     item.addEventListener("click", function (e) {
-        result += parseInt(item.innerHTML);
-        calculatorScreen.textContent = String(result);
+        e.preventDefault();
+        if (item.id === allClearButton.id) {
+            result = '';
+            // 초기화 후 리렌더
+            location.reload();
+        }
+        else {
+            result += item.innerHTML;
+            calculatorScreen.textContent = String(result);
+        }
     });
+});
+var AddButton = document.querySelector("#Add");
+// 더하기 버튼
+AddButton.addEventListener("click", function () {
+    A = parseInt(result);
+    calculatorSubScreen.textContent = String(A);
+    calculatorScreen.textContent = "";
+});
+// 결과 버튼
+total.addEventListener("click", function () {
+    if ()
+        ;
 });
--- src/script.ts
+++ src/script.ts
@@ -1,10 +1,43 @@
+// 스크린 영역
 let calculatorScreen = document.querySelector(".calculator__screen");
+let calculatorSubScreen = document.querySelector(".calculator__sub-screen");
+// 스크린 영역 안 버튼
 let padButton = document.querySelectorAll(".calculator__table tbody td button");
+// 총합
+let total = document.querySelector("#total");
 
-let result = 0;
+let allClearButton = document.querySelector("#AllClear");
+let A = 0;
+
+let B = 0
+// 초기화
+let result = '';
+// 버튼 반복 돌리면서 클리이벤트 걸어주기
 padButton.forEach((item, i) => {
-    item.addEventListener("click", (e) => {
-        result += parseInt(item.innerHTML);
-        calculatorScreen.textContent = String(result);
-    })
+  item.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (item.id === allClearButton.id) {
+      result = '';
+      // 초기화 후 리렌더
+      location.reload();
+    } else {
+      result += item.innerHTML;
+      calculatorScreen.textContent = String(result);
+    }
+  })
+
+});
+let AddButton = document.querySelector("#Add");
+
+// 더하기 버튼
+AddButton.addEventListener("click", () => {
+  A = parseInt(result);
+  calculatorSubScreen.textContent = String(A);
+  calculatorScreen.textContent = "";
+});
+
+// 결과 버튼
+total.addEventListener("click", () => {
+  if ()
 })
+
